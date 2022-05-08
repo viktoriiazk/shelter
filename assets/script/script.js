c6ab82666a199ef9fc2499be98d35363382ddb29
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2022-05-08 23:21:05 +0200

    fix: resolve issue with pagination

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,8 +13,6 @@ const nextPage = document.getElementById('nextPage');
 const lastPage = document.getElementById('lastPage');
 const pageNumber = document.querySelector('.pagination-number');
 const petsCard = document.querySelectorAll('.pets-card');
-const petsCardName = document.querySelectorAll('.pets-card__name');
-const petsCardImg = document.querySelectorAll('.pets-card__img');
 let count = pageNumber.innerHTML;
 
 
@@ -28,7 +26,14 @@ prevPage.addEventListener("click", () => {
         firstPage.dataset.disabled = 'false';
         pageNumber.innerHTML = count;
 
-    } else {
+    } else if( count == 1){
+        nextPage.dataset.disabled = 'false';
+        lastPage.dataset.disabled = 'false';
+        prevPage.dataset.disabled = 'true';
+        firstPage.dataset.disabled = 'true';
+        pageNumber.innerHTML = count;
+
+    }else {
         count++;
         prevPage.dataset.disabled = 'true';
         firstPage.dataset.disabled = 'true';
@@ -74,7 +79,13 @@ nextPage.addEventListener("click", () => {
         prevPage.dataset.disabled = 'false';
         firstPage.dataset.disabled = 'false';
         pageNumber.innerHTML = count;
-        cardsDesktop += 8;
+    } else if (count==5){
+        nextPage.dataset.disabled = 'true';
+        lastPage.dataset.disabled = 'true';
+        prevPage.dataset.disabled = 'false';
+        firstPage.dataset.disabled = 'false';
+        pageNumber.innerHTML = count;
+
     }
     else {
         count--;
@@ -85,17 +96,6 @@ nextPage.addEventListener("click", () => {
         pageNumber.innerHTML = count;
     }
 
-    console.log(cardsDesktop);
-    [...pets].forEach((pets, index) => {
-
-
-        const petsCardItemName = petsCardName[index];
-        const petsCardItemImg =  petsCardImg[index];
-        if (petsCardItemName !== undefined && petsCardItemImg !== undefined ) {
-            petsCardItemName.innerText = this.name;
-            petsCardItemImg.src = this.img;
-        }
-    });
 
 });
 
